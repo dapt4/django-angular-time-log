41e8d76022d87b59352713cf527c749ced1c44c8
dapt <diegodapt18@gmail.com>
2025-02-03 20:21:44 +0100

    adding @api_view decorator

--- timelog_backend/timelog/views.py
+++ timelog_backend/timelog/views.py
@@ -1,7 +1,9 @@
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import api_view
 
 
 # Create your views here.
+@api_view(('GET',))
 def hello_world(request):
     return Response(data={'hello': 'world'}, status=status.HTTP_201_OK)
