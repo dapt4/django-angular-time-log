691a663e3f888c4cbeef376d7b7d3e7cf5968b0c
dapt <diegodapt18@gmail.com>
2025-02-21 19:31:24 +0100

    logrado el registro y el login en la app

--- timelog_backend/timelog/views.py
+++ timelog_backend/timelog/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
 # Create your views here.
@@ -30,22 +30,21 @@ def login(request):
         pss = request.data['password']
         user = User.objects.get(username=usr, password=pss)
         if user:
-            token, created = Token.objects.create(user=user)
-            print(token.key)
-            print(created)
-            if created:
-                return Response(data={'token': token.key},
-                                status=status.HTTP_200_OK)
+            token, created = Token.objects.get_or_create(user=user)
+            return Response(data={'token': token.key},
+                            status=status.HTTP_200_OK)
         else:
             return Response(
                 data={'error': "username and password don't match"},
                 status=status.HTTP_404_NOT_FOUND)
     except Exception as err:
+        print(err)
         return Response(
-            data={'error': err}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            data={'error': 'hubo un error'},
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 @api_view(('GET',))
 @permission_classes([IsAuthenticated])
-def hello_world():
+def hello_world(request):
     return Response(data={'hello': 'world'}, status=status.HTTP_200_OK)
